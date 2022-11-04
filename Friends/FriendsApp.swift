803e4d24779f29c336fdc51ad6642ca0a9d07f6a
Alison Gorman <alison@minimascientific.com>
2022-11-04 07:36:37 -0600

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Alison Gorman on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
